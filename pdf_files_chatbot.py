92c6a5be9c45bfb3fcabb264f32dc2e8f4a7187a
Ganesh Patil <ganeshkumar.patil@gmail.com>
2025-03-29 16:53:23 +0000

    update

--- pdf_files_chatbot.py
+++ pdf_files_chatbot.py
@@ -72,7 +72,7 @@ if uploaded_files:
             
             system_prompt = (
                 "You are an assistant for question-answering tasks. "
-                "Use the following pieces of retrieved context to answer "
+                "Use the following pieces of retrieved context only to answer "
                 "the question. If you don't know the answer, say that you "
                 "don't know. Use three sentences maximum and keep the "
                 "answer concise.\n\n{context}"
